36e7deb626329be4bff1f082de9170b6575190a4
Benedikt Schnatterbeck <benediktms@hey.com>
2023-01-16 11:03:22 +0000

    Use script to install gnu utils and add the to path on macOs

---
+++ use-gnu.sh
@@ -0,0 +1,13 @@
+brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
+
+echo 'export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gawk/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gnutls/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gnu-indent/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"' >> ~/.zshrc
+
+
